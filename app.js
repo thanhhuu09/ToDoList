95812156c3095ac5fc2d4e91e0db33e9bac2529a
Tommy Thach <thachtommy5@gmail.com>
2023-03-01 20:24:06 +0700

    update var to let

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
 
 const app = express();
-var listItems = [];
+
+let listItems = [];
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res){
-    var today = new Date();
-    const options = {
+    let today = new Date();
+    let options = {
         weekday: "long",
         year: "numeric",
         month: "long",
         day: "numeric",
     };
-    var currentDay = today.toLocaleDateString("en-US", options);
+    let currentDay = today.toLocaleDateString("en-US", options);
     res.render("list", {kindOfDay:currentDay, item:listItems});
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     listItems.push(item);
     res.redirect("/");
 });
+
 app.listen(3000, function(){
     console.log("Server start on port 3000");
 });
